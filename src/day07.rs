8999de67b5f197c8cf27249c0b9acb404320a83d
Guilhermebs <guilhermebs@drcmr.dk>
2022-07-20 21:22:06 +0200

    run linter

--- src/day07.rs
+++ src/day07.rs
@@ -1,7 +1,9 @@
-use std::{collections::{HashMap, BTreeSet}, fs};
+use std::{
+    collections::{BTreeSet, HashMap},
+    fs,
+};
 
 use regex::Regex;
-
 
 pub fn day07() {
     let mut with_bag = HashMap::<String, Vec<(u32, String)>>::new();
@@ -11,37 +13,44 @@ pub fn day07() {
     let re_contains: Regex = Regex::new(r"(\d) ([ \w]+) bags?").unwrap();
     for rule in buffer.split("\n").filter(|s| s.len() > 0) {
         match re_name.captures(rule) {
-           Some(c1) => {
+            Some(c1) => {
                 for c2 in re_contains.captures_iter(rule) {
-                    with_bag.entry(c2[2].to_string())
-                        .or_default().push((c2[1].parse::<u32>().unwrap(), c1[1].to_string()));
-                    bags_in.entry(c1[1].to_string())
-                        .or_default().push((c2[1].parse::<u32>().unwrap(), c2[2].to_string()));
-
+                    with_bag
+                        .entry(c2[2].to_string())
+                        .or_default()
+                        .push((c2[1].parse::<u32>().unwrap(), c1[1].to_string()));
+                    bags_in
+                        .entry(c1[1].to_string())
+                        .or_default()
+                        .push((c2[1].parse::<u32>().unwrap(), c2[2].to_string()));
                 }
             }
-           None =>  println!("Could not find bag name")
+            None => println!("Could not find bag name"),
         }
     }
 
     let mut part1_sol = 0;
     let mut searched = BTreeSet::<&String>::new();
     let mut search_stack = Vec::<&String>::from_iter(
-        with_bag.get("shiny gold").unwrap()
-            .iter().map(|(_n, bag)| bag));
+        with_bag
+            .get("shiny gold")
+            .unwrap()
+            .iter()
+            .map(|(_n, bag)| bag),
+    );
 
-    while search_stack.len() > 0  {
+    while search_stack.len() > 0 {
         part1_sol += 1;
         let bag = search_stack.pop().unwrap();
         match with_bag.get(bag) {
             Some(iter) => {
                 for (_n, b) in iter {
-                    if !searched.contains(b) && ! search_stack.contains(&b) {
+                    if !searched.contains(b) && !search_stack.contains(&b) {
                         search_stack.push(b)
                     }
                 }
-            },
-            None => ()
+            }
+            None => (),
         }
         searched.insert(bag);
     }
@@ -51,14 +60,16 @@ pub fn day07() {
     let mut part2_sol = 0;
     let mut ammount_and_bags = bags_in.get("shiny gold").unwrap().clone();
     while ammount_and_bags.len() > 0 {
-        let (n, b) = ammount_and_bags.pop().unwrap(); 
+        let (n, b) = ammount_and_bags.pop().unwrap();
         part2_sol += n;
         ammount_and_bags.extend(
-            bags_in.entry(b).or_default().iter()
-                .map(|(n_in, b_in)| (n*n_in, b_in.to_string()))
+            bags_in
+                .entry(b)
+                .or_default()
+                .iter()
+                .map(|(n_in, b_in)| (n * n_in, b_in.to_string())),
         );
     }
 
     println!("Part 2: {:?}", part2_sol);
-
 }
