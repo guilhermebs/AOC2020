92b8e3f31c58f4935c6d36c5c9b715f5f00648ba
Guilhermebs <guilhermebs@drcmr.dk>
2022-07-30 07:57:14 +0200

    completed day 08

--- src/day08.rs
+++ src/day08.rs
@@ -22,7 +22,6 @@ pub fn day08() {
 
     loop {
         let (inst, val) = &instructions[cur_instr];
-        println!("{:?}, {:?}, {:?}", cur_instr, inst, val);
         n_visits[cur_instr] += 1;
         if n_visits.iter().any(|&n| n > 1) {
             break;
@@ -37,5 +36,54 @@ pub fn day08() {
         }
     }
 
-    println!("{:?}", part1_sol);
+    println!("Part 1 solution: {:?}", part1_sol);
+
+    let mut can_flip = instructions.iter().map(|(instr, _)| instr == "jmp" || instr == "nop").collect::<Vec<bool>>();
+    let mut part2_sol: i32;
+    loop {
+        let mut visited = vec![false; instructions.len()];
+        let mut has_flipped = false;
+        cur_instr = 0;
+        part2_sol = 0;
+        loop {
+            if cur_instr >= instructions.len() || visited[cur_instr] {
+                break;
+            }
+            visited[cur_instr] = true;
+            let (inst, val) = &instructions[cur_instr];
+            let mut instruction: String = inst.to_owned();
+            if !has_flipped {
+                let (inst2, flip) = maybe_flip(inst, &can_flip[cur_instr]);
+                has_flipped = flip;
+                if flip { 
+                    can_flip[cur_instr] = false;
+                };
+                instruction = inst2;
+            }
+            match instruction.as_str() {
+                "acc" => {
+                    part2_sol += val;
+                    cur_instr += 1
+                }
+                "jmp" => cur_instr = (cur_instr as i32 + val) as usize,
+                _ => cur_instr += 1,
+            }
+        }       
+        if *visited.last().unwrap() { 
+            break;
+        }
+    }
+    println!("Part 2: {:?}", part2_sol);
+
+}
+
+fn maybe_flip(inst: &String, can_flip: &bool) -> (String, bool) {
+    if !can_flip {
+        return (inst.to_owned(), false);
+    }
+    match inst.as_str() {
+       "jmp" => return ("nop".to_string(), true),
+       "nop" => return ("jmp".to_string(), true),
+       _ => return(inst.to_owned(), false)
+    }
 }
