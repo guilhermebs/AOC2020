344c19fe22927c2a121fefb6193b8bd09dd7a54a
Guilhermebs <guilhermebs@drcmr.dk>
2022-09-10 14:59:27 +0200

    day 13, part 1

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ mod day09;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 
 fn main() {
     let start = Instant::now();
@@ -29,7 +30,8 @@ fn main() {
     //day09::day09();
     //day10::day10();
     //day11::day11();
-    day12::day12(); 
+    //day12::day12(); 
+    day13::day13(); 
     let duration = start.elapsed();
     println!("Time elapsed: {:?}", duration)
 }
