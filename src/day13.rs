fde7bb8570ced6f8cd071f1f1c4b173b7064d422
Guilhermebs <guilhermebs@drcmr.dk>
2022-09-11 11:29:36 +0200

    day12, part2

--- src/day13.rs
+++ src/day13.rs
@@ -1,5 +1,5 @@
 use std::fs;
-
+use gcd::Gcd;
 use itertools::Itertools;
 
 
@@ -11,14 +11,12 @@ fn find_bus_at_time(buses: &Vec<Option<u64>>, time: u64) -> Vec<u64> {
 }
 
 
-fn validate(buses: &Vec<Option<u64>>, time: u64) -> bool {
-    for (i, bus) in buses.iter().enumerate() {
-        match bus {
-            Some(n) => {if (time + i as u64) % n != 0 { return false }},
-            None => ()
-        };
+fn update_t_init_and_step_size(t_init: u64, step_size: u64, period: u64, offest: usize) -> (u64, u64) {
+    let mut t = t_init;
+    while (t + offest as u64) % period != 0 {
+        t += step_size;
     }
-    true
+    (t, (step_size * period)/step_size.gcd(period))
 }
 
 #[allow(dead_code)]
@@ -26,7 +24,6 @@ pub fn day13() {
     let t_init = 1001938u64;
     let buses = fs::read_to_string("inputs/day13").unwrap().split('\n')
         .nth(1).unwrap().split(',').map(|x| x.parse::<u64>().ok()).collect::<Vec<Option<u64>>>();
-    println!("{:?}", buses);
     let mut t = t_init;
     let mut bus_t: Vec<u64>;
     loop {
@@ -38,13 +35,13 @@ pub fn day13() {
     println!("Part 1: {:?}", part1_sol);
 
     //let buses = vec![Some(67u64),Some(7u64),None,Some(59u64),Some(61u64)];
-    let step_size: u64 = buses.iter().max().unwrap().unwrap() as u64;
-    let mut t: u64 = step_size - buses.iter().position_max().unwrap() as u64;
-    let mut i = 0;
-    while !validate(&buses, t) {
-        if i%1000000 == 0 { println!("t={:?}", t) };
-        t += step_size;
-        i += 1;
+    let mut step_size: u64 = 1;
+    let mut t_init: u64 = 0;
+    for (offset, bus) in buses.iter().enumerate() {
+        match bus {
+            Some(period) => { (t_init, step_size) = update_t_init_and_step_size(t_init, step_size, *period, offset); },
+            None => ()
+        }
     }
-    println!("Part 2: {:?}", t);
+    println!("Part 2: {:?}", t_init);
 }
