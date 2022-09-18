0ecb8226144dc92ebda65ac0d179607a2b2f3a62
Guilhermebs <guilhermebs@drcmr.dk>
2022-09-18 15:33:43 +0200

    day16, part 1

---
+++ src/day16.rs
@@ -0,0 +1,78 @@
+use regex::Regex;
+use std::fs;
+
+#[allow(dead_code)]
+#[derive(Copy, Clone, Debug)]
+struct Interval {
+    lb: usize,
+    ub: usize,
+}
+
+impl Interval {
+    pub fn is_in(self: &Interval, nr: &usize) -> bool {
+        return self.lb <= *nr && *nr <= self.ub;
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct TicketField {
+    name: String,
+    intervals: [Interval; 2],
+}
+
+impl TicketField {
+    pub fn from_string(ln: &str) -> TicketField {
+        let re = Regex::new(r"([\w\s]+): (\d+)-(\d+) or (\d+)-(\d+)$").unwrap();
+        match re.captures(ln) {
+            Some(c) => TicketField {
+                name: c[1].to_string(),
+                intervals: [
+                    Interval {
+                        lb: c[2].parse::<usize>().unwrap(),
+                        ub: c[3].parse::<usize>().unwrap(),
+                    },
+                    Interval {
+                        lb: c[4].parse::<usize>().unwrap(),
+                        ub: c[5].parse::<usize>().unwrap(),
+                    },
+                ],
+            },
+
+            None => panic!("invalid string: {:?}", ln),
+        }
+    }
+
+    pub fn evaluate(self: &TicketField, nr: &usize) -> bool {
+        self.intervals.iter().any(|interval| interval.is_in(nr))
+    }
+}
+
+#[allow(dead_code)]
+pub fn day16() {
+    let file_contents = fs::read_to_string("inputs/day16").unwrap();
+    let mut blocks = file_contents.split("\n\n");
+
+    let rules_str = blocks.next().unwrap();
+    let my_ticket_str = blocks.next().unwrap();
+    let nearby_tickets_str = blocks.next().unwrap();
+
+    let rules = rules_str
+        .split("\n")
+        .map(|ln| TicketField::from_string(ln))
+        .collect::<Vec<TicketField>>();
+
+    let mut nearby_tickets = nearby_tickets_str.split("\n");
+    nearby_tickets.next();
+    let mut part1_sol = 0;
+    for ticket in nearby_tickets.filter(|t| t.len() > 0) {
+        println!("{:?}", ticket);
+        part1_sol += ticket
+            .split(",")
+            .map(|x| x.parse::<usize>().unwrap())
+            .filter(|x| !rules.iter().any(|r| r.evaluate(&x)))
+            .sum::<usize>();
+    }
+
+    println!("Part 1: {:?}", part1_sol);
+}
--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ mod day12;
 mod day13;
 mod day14;
 mod day15;
+mod day16;
 
 fn main() {
     let start = Instant::now();
@@ -35,7 +36,8 @@ fn main() {
     //day12::day12(); 
     //day13::day13(); 
     //day14::day14(); 
-    day15::day15(); 
+    //day15::day15(); 
+    day16::day16(); 
     let duration = start.elapsed();
     println!("Time elapsed: {:?}", duration)
 }
